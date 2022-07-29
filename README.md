661ef741b88e497fefad22a665a6478427034d5c
Hierlmeier, Richard <richard.hierlmeier@de-gmbh.com>
2022-07-29 13:29:50 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# karaf-servlet-context
-Demo for setting the ServletConextHelper for Web elements in Karaf
+# Test project for PAX-WEB 8
+
+## Submodules:
+
+* paxweb8-test-mix-wab-and-whiteboard: Test that mixes WAB and Http Whiteboard
---
+++ paxweb8-test-mix-wab-and-whiteboard/src/main/java/de/hierlmeier/paxweb8test/mixWabAndWhiteboard/RootRedirectFilter.java
@@ -0,0 +1,42 @@
+package de.hierlmeier.paxweb8test.mixWabAndWhiteboard;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Component(
+        property = {HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_PATTERN + "=/*"}
+)
+public class RootRedirectFilter implements Filter {
+
+    private final Logger m_Logger = Logger.getLogger(RootRedirectFilter.class.getName());
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if(servletRequest instanceof HttpServletRequest) {
+            HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+            m_Logger.info("doFilter " + httpRequest.getRequestURI());
+            if("/".equals(httpRequest.getRequestURI())) {
+                HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+                httpServletResponse.sendRedirect("/wab");
+                return;
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
